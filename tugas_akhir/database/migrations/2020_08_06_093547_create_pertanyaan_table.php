71dab5ea84730f6cac9ed8f9c48e2561ed363c14
Vito Ramadhan <vitorama98@gmail.com>
2020-08-08 22:12:27 +0700

    first push laravel project

---
+++ tugas_akhir/database/migrations/2020_08_06_093547_create_pertanyaan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePertanyaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertanyaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul', 45);
+            $table->string('isi', 255);
+            $table->timestamp('tanggal_dibuat')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('tanggal_diperbaharui')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertanyaan');
+    }
+}
